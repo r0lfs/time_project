8f2b1856707ab959c508302cf3b8ae0ad54d99bd
r0lfs <rdwoolen@gmail.com>
2017-10-08 14:41:44 -0600

    changed variable names and cleaned up code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const clock = document.getElementById('clockDisplay');
 const hex = document.getElementById('hexDisplay');
-const colors = document.getElementById('colors');
+const body = document.getElementById('body');
 
 //displays time in digital and as a hexcode and changes background
 function timeDisplay() {
@@ -9,34 +9,30 @@ function timeDisplay() {
 	let m = underTen(localTime.getMinutes());
 	let s = underTen(localTime.getSeconds());
 
-	// let trueH = localTime.getHours();
-	// let trueM = localTime.getMinutes();
-	// let trueS = localTime.getSeconds();
-
 	//changes clock to 12 hour format
 	if (h < 12) {
 		clock.innerHTML = `Current Local Time: ${h}:${m}:${s} A.M.`;
 	}else if (h===12){
 		clock.innerHTML = `Current Local Time: ${h}:${m}:${s} P.M.`;
 	}else{
-		let newH = h - 12;
-		newH = underTen(newH);
+		let newH = underTen(h - 12);
 		clock.innerHTML = `Current Local Time: ${newH}:${m}:${s} P.M.`;
 	}
 
-	//displays clock in hex format and changes background colors
-	let hexColor = `${h}${m}${s}`;
-	let hexCode = parseInt(hexColor);
-	colors.style.background = `#${hexColor}`;
+	//changes background color
+	let backColor = `${h}${m}${s}`;
+	body.style.background = `#${backColor}`;
 
 	// inverts the color of the text relative to the background
-	let textColor = hexInvert(hexColor);
-	colors.style.color = `#${textColor}`;
+	let textColor = hexInvert(backColor);
+	body.style.color = `#${textColor}`;
 
-	hex.innerHTML = `The background hex color code is:#${hexColor} <br> The inverted text color code is: #${textColor}`;
-	//keeps clock updated every half second
-	const updated = setTimeout(timeDisplay, 500);
-}
+	//displays the hex codes for background and text
+	hex.innerHTML = `The background hex color code is:#${backColor} <br> The inverted text color code is: #${textColor}`;
+
+	//keeps clock updated every half second by calling timeDisplay recursively at the end of each execution.
+	setTimeout(timeDisplay, 500);
+}//end timeDisplay function
 
 //adds a 0 before number if under ten
 function underTen(x) {
@@ -48,13 +44,13 @@ function underTen(x) {
 
 //text color inverter; inverts the hexCode for the color of text
 function hexInvert(num) {
-	let splitNum = num.toString().split('');
+	let splitNum = num.split('');
 	let baseArray = "0123456789".split('');
 	let inverseArray = "FEDCBA9876".split('') 
 	let hexArray = [];
 	let inverseCode = '';
 
-	for ( i = 0; i < splitNum.length; i++) {
+	for (i = 0; i < splitNum.length; i++) {
 		let newCode = baseArray.indexOf(splitNum[i]);
 		hexArray.push(inverseArray[newCode]);
 	}
@@ -63,3 +59,4 @@ function hexInvert(num) {
 	return inverseCode;
 }
 
+timeDisplay();
